afd07da5211ca7e637e504125c169ee4ab792f1d
sayam9901 <sayamkhan9560621912@gmail.com>
2023-10-09 18:14:22 +0530

    implementing the post req in the code

--- src/component/item/ItemForm.js
+++ src/component/item/ItemForm.js
@@ -1,5 +1,6 @@
 // ItemForm.js
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function ItemForm({ addToItems }) {
   const [name, setName] = useState('');
@@ -12,6 +13,14 @@ function ItemForm({ addToItems }) {
     const newItem = { name, description, price, quantity };
     addToItems(newItem);
     // Clear the form fields
+    axios.post('https://crudcrud.com/api/651488945048411aa2fc4ac9eec50a47/items', newItem)
+    .then((response) => {
+      // addToItems(response.data); // Add the new item to the local state
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('Error posting data:', error);
+    });
     setName('');
     setDescription('');
     setPrice(0);
